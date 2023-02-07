8c615066c5ac151a25edb02947b6340087cd4fb8
MD. RAFIUL ISLAM <islam15-5320@diu.edu.bd>
2023-02-07 20:10:51 +0600

    Create Assignment01.java

---
+++ Assignment01.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project01;
+
+/**
+ *
+ * @author PRANTO
+ */
+public class Assignment01 {
+    public static void main(String[] args) {
+        //Dispaly some information in java
+        System.out.println("Hi, I'm RAFI\nI'm working on my lab task");
+       int x = 10, y = 15, res = x+y;
+       System.out.println(res);
+       
+       //Home work 01
+       System.out.println("\n");
+       String name = "MD. RAFIUL ISLAM";
+       String id = "221-15-5320";
+       String adrs = "JOYPURHAT, RAJSHAHI";
+       String dep = "CSE";
+       String fgame = "FOOTBALL";
+       System.out.println("Name: "+name);
+       System.out.println("ID: "+id);
+       System.out.println("Address: "+adrs);
+       System.out.println("Department: "+dep);
+       System.out.println("Favourite Game: "+fgame);
+       
+       //Home work 02
+       System.out.println("\n");
+       int a = 10, b = 5;
+      int result1 = a+b;
+       int result2 = a-b;
+       int result3 = a*b;
+       int result4 = a/b;
+       System.out.println("Summation: "+result1);
+       System.out.println("Subtraction: "+result2);
+       System.out.println("Multiplication: "+result3);
+       System.out.println("Devision: "+result4);
+       
+       //Home work 03
+       System.out.println("\n");
+       String st = "MD. RAFIUL ISLAM";
+       System.out.println("Name: "+st);
+       
+       //Home work 04
+       System.out.println("\n");
+       double r = 3, result;
+       result  = 3.1416 *r*r;
+       System.out.printf("Area of circle: %.2f", result);
+       
+       //Home work 05
+       System.out.println("\n");
+       int A=5,B=6,C=7,D=8;
+       int r1 = (A*B - C*D);
+       int r2 = (2*A - B + 3*D);
+       int r3 = (A*A+B*B - C*C+D*D);
+       int r4 = (A*A*A+B-C*C);
+       System.out.println("a="+r1);
+       System.out.println("b="+r2);
+       System.out.println("c="+r3);
+       System.out.println("d="+r4);
+       
+       
+       
+        
+    }
+    
+}
+
+    
+    
+
